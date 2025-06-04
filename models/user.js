052c6c9de82b7885be8156dcbc9e64a775c94e13
Jorge Andrés Dueñas Almeida <jorge.duenas@epn.edu.ec>
2025-06-04 17:11:21 -0500

    feat: agregar modelo user.js

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    weight: { type: Number },
+    height: { type: Number },
+    goals: { type: String },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
+}, { timestamps: true });
+
+// Hash password antes de guardar
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// Método para comparar passwords
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
